974185ca3845b517e3ce0c195c2832db311352e7
Александр Пономарев <aponomarev@tpar.local>
2019-04-17 17:28:41 +0300

    test fs

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,5 +1,6 @@
 var WebSocketServer = new require('ws');
 const { exec } = require('child_process');
+const fs = require('fs');
 
 var webSocketServer = new WebSocketServer.Server({
   port: 8081
@@ -15,6 +16,7 @@ webSocketServer.on('connection', function(ws) {
 	
   console.log('получено сообщение');
   console.log(message);
+  fs.writeFileSync(message, "files write!")
 	//exec("lp /home/ubuntu/devjs/1/webtoprint/data/test.pdf", function (error, stdout, stderr) {
 	});
 
